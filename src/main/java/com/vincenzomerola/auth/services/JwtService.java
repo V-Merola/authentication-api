1d8c02b4538187b45e95f2797dad64abfcd20e27
V-Merola <vincenzo.merola18@gmail.com>
2024-05-18 17:09:14 +0200

    add Role in Token

--- src/main/java/com/vincenzomerola/auth/services/JwtService.java
+++ src/main/java/com/vincenzomerola/auth/services/JwtService.java
@@ -14,6 +14,7 @@ import java.util.function.Function;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import com.vincenzomerola.auth.entities.*;
 
 @Service
 public class JwtService {
@@ -35,11 +36,22 @@ public class JwtService {
     public String generateToken(UserDetails userDetails) {
         return generateToken(new HashMap<>(), userDetails);
     }
-
+/*
     public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
-        return buildToken(extraClaims, userDetails, jwtExpiration);
-    }
 
+    	return buildToken(extraClaims, userDetails, jwtExpiration);
+    }
+*/
+    //----------------
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>(extraClaims);
+        claims.put("role", ((User) userDetails).getRole().toString()); // Aggiungi il ruolo come claim
+
+        return buildToken(claims, userDetails, jwtExpiration);
+    }
+    //----------------
+
+    
     public long getExpirationTime() {
         return jwtExpiration;
     }
@@ -59,10 +71,24 @@ public class JwtService {
                 .compact();
     }
 
+    /*
     public boolean isTokenValid(String token, UserDetails userDetails) {
         final String username = extractUsername(token);
         return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
     }
+	*/
+    //----------------
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        final String tokenRole = extractClaim(token, claims -> claims.get("role", String.class)); // Estrai il ruolo dal token
+        final String userRole = ((User) userDetails).getRole().toString(); // Ottieni il ruolo dell'utente
+
+        return (username.equals(userDetails.getUsername())) && 
+               (tokenRole.equals(userRole)) && // Confronta i ruoli
+               !isTokenExpired(token);
+    }
+    
+    //----------------
 
     private boolean isTokenExpired(String token) {
         return extractExpiration(token).before(new Date());
